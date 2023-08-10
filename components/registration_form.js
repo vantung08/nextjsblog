a1be39e05bfe8b5c319179bd93cc4b54a99d0213
tungtv <tungtv@Ubuntu-01.myguest.virtualbox.org>
2023-08-10 13:44:16 +0700

    Update registration page

--- components/registration_form.js
+++ components/registration_form.js
@@ -1,28 +1,48 @@
-const handleSubmit = async(e) => {
-    e.preventDefault();
-    const response = await fetch('http://localhost:5000/POST/users');
-    if (response.ok) {
-        console.log('Authorization successful');
-    } else {
-        console.log('Authorization failed');
-    }
-};
+import { useState } from "react";
 
 
-export default function RegistrationForm() {
+function RegistrationForm() {
+    const[email, setEmail] = useState('')
+    const[password, setPassword] = useState('')
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const user = {email, password};
+        try {
+            const response = await fetch('http://localhost:5000/POST/users', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log('User registered successfully: ', data);
+            } else {
+                console.log('Registration failed: ', response.statusText);
+            }
+        } catch(error) {
+            console.log('Error occurred during registration: ', error);
+        }
+    };
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="firstName">First Name:</label>
+            {/* <label htmlFor="firstName">First Name:</label>
             <input type="text" id="firstName" name="firstName" required/>
             <label htmlFor="lastName">Last Name:</label>
-            <input type="text" id="lastName" name="lastName" required/><br/>
+            <input type="text" id="lastName" name="lastName" required/><br/> */}
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required/><br/>
+            <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required/><br/>
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required/><br/>
-            <label htmlFor="confirmPassword">Confirm Password:</label>
-            <input type="password" id="confirmPassword" name="confirmPassword" required/><br/>
+            <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required/><br/>
+            {/* <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input type="password" id="confirmPassword" name="confirmPassword" required/><br/> */}
             <button type="submit">Sign up</button>
         </form>
     );
 };
+
+
+export default RegistrationForm
